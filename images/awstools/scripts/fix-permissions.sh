8e33bb7a568a4660a83dfed1bddc5daa7edbe564
Florian JUDITH <florian.judith@alithya.com>
2024-10-23 19:43:54 -0400

    Clarified variables name

--- images/awstools/scripts/fix-permissions.sh
+++ images/awstools/scripts/fix-permissions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Set permissions on a directory
 # After any installation, if a directory needs to be (human) user-writable, run this script on it.
-# It will make everything in the directory owned by the group ${NB_GID} and writable by that group.
+# It will make everything in the directory owned by the group ${NON_ROOT_GID} and writable by that group.
 # Deployments that want to set a specific user id can preserve permissions
 # by adding the `--group-add users` line to `docker run`.
 
@@ -9,7 +9,7 @@
 # which would cause a massive image explosion
 
 # Right permissions are:
-# group=${NB_GID}
+# group=${NON_ROOT_GID}
 # AND permissions include group rwX (directory-execute)
 # AND directories have setuid,setgid bits set
 
@@ -18,10 +18,10 @@ set -e
 for d in "$@"; do
     find "${d}" \
         ! \( \
-            -group "${NB_GID}" \
+            -group "${NON_ROOT_GID}" \
             -a -perm -g+rwX \
         \) \
-        -exec chgrp "${NB_GID}" -- {} \+ \
+        -exec chgrp "${NON_ROOT_GID}" -- {} \+ \
         -exec chmod g+rwX -- {} \+
     # setuid, setgid *on directories only*
     find "${d}" \
